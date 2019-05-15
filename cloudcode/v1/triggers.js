7703014a836e0ac37e52bab0c78a1138e1db6df4
Leonardo Reis <leonardobsr@gmail.com>
2019-05-14 23:44:09 -0300

    teste duplicidade 3

--- cloudcode/v1/triggers.js
+++ cloudcode/v1/triggers.js
@@ -1,24 +1,24 @@
 const _ = require ('underscore');
 const helpers = require ('./helpers.js');
 
-Parse.Cloud.beforeSave("Balance", function (request, response) {
+Parse.Cloud.beforeSave("Balance", async function (request) {
   if (request.object.isNew()) {
     let Balance = Parse.Object.extend("Balance");
     let query = new Parse.Query(Balance);
     query.equalTo("user", request.object.get('user'));
     query.equalTo("year", request.object.get('year'));
     query.equalTo("month", request.object.get('month'));
-    query.first({
+    return await query.first({
       success: function(object) {
         if (!object) {
           object = request.object.save();
         } else {
           object.set("expense", request.object.get('expense'));
         }
-        response.success(object);
+        return object;
       },
       error: function(error) {
-        response.error("Error: " + error.code + " " + error.message);
+        throw "Error: " + error.code + " " + error.message;
       }
     });
   }
